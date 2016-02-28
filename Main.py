8b22a90a290b0964fc496cc3042072dfcfa19003
Maegner <franciscommaguiar@gmail.com>
2016-02-28 20:26:47 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -143,6 +143,33 @@ def select_mode():
 		pygame.display.update()
 		clock.tick(15)
 
+def help_screen():
+	global reading
+	while reading:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+		gameDisplay.blit(intro_background, (0, 0))
+		screen_text("Comandos:", 50, 110, 30 ,black )
+		screen_text("A - esquerda", 50, 140, 30 ,black )
+		screen_text("D - direita", 50, 170, 30 ,black )
+		screen_text("UP - disparar", 50, 200, 30 ,black )
+		screen_text("Sistema das ECT's", 50, 270, 30 ,black )
+		screen_text("Deixas a disciplina passar: ", 60, 310, 25 ,black )
+		screen_text("Verde:-2 ECT's ", 70, 340, 20 ,black )
+		screen_text("Azul:-4 ECT's ", 70, 370, 20 ,black )
+		screen_text("Vermelha:+3 ECT's ", 70, 400, 20 ,black )
+		screen_text("Atinges a disciplina: ", 60, 440, 25 ,black )
+		screen_text("Verde:+2 ECT's ", 70, 470, 20 ,black )
+		screen_text("Azul:+10 ECT's ", 70, 500, 20 ,black )
+		screen_text("Vermelha:+10 ECT's (mas e imortal) ", 70, 530, 20 ,black )
+		screen_text_center("Horario de Duvidas", display_width/2-2, display_height/12-1, 50, black)
+		screen_text_center("Horario de Duvidas", display_width/2, display_height/12, 50, white)
+		button("Voltar",550,450,200,100,linen,linen,40,game_intro,0)
+
+		pygame.display.update()
+		clock.tick(15)
 
 def deathscreen(new_score, name, old_score):
 	
